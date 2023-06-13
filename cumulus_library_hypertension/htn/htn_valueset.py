cc673370f9f687295920539f2f3c7eaddef9f6b6
comorbidity <andrew.mcmurry@tch.harvard.edu>
2023-06-13 15:00:57 -0700

    htn_valueset.py updated to support inclusion of new valuesets.

--- cumulus_library_hypertension/htn/htn_valueset.py
+++ cumulus_library_hypertension/htn/htn_valueset.py
@@ -3,7 +3,30 @@ from typing import List
 from cumulus_library.helper import load_json
 from fhirclient.models.coding import Coding
 
-def create_view(view_name: str, concept_list: List[Coding]) -> str:
+def include_coding(valueset_json) -> List[Coding]:
+    """
+    Obtain a list of Coding "concepts" from a ValueSet.
+    This method currently supports only "include" of "concept" defined fields.
+    Not supported: recursive fetching of contained ValueSets, which requires UMLS API Key and Wget, etc.
+
+    examples
+    https://vsac.nlm.nih.gov/valueset/2.16.840.1.113762.1.4.1146.1629/expansion/Latest
+    https://cts.nlm.nih.gov/fhir/res/ValueSet/2.16.840.1.113762.1.4.1146.1629?_format=json
+
+    :param valueset_json: ValueSet file, expecially those provided by NLM/ONC/VSAC
+    :return: list of codeable concepts (system, code, display) to include
+    """
+    valueset = load_json(get_path(valueset_json))
+    parsed = list()
+
+    for include in valueset['compose']['include']:
+        if 'concept' in include.keys():
+            for concept in include['concept']:
+                concept['system'] = include['system']
+                parsed.append(Coding(concept))
+    return parsed
+
+def create_view_sql(view_name: str, concept_list: List[Coding]) -> str:
     """
     :param view_name: like study__define_type
     :param concept_list: list of concepts to include in definition
@@ -17,34 +40,33 @@ def create_view(view_name: str, concept_list: List[Coding]) -> str:
     content = '\n,'.join(content)
     return header + '\n' + content + '\n' + footer
 
+def write_view_sql(view_name: str, concept_list: List[Coding]) -> None:
+    with open(get_path(f'{view_name}.sql'), 'w') as fp:
+        fp.write(create_view_sql(view_name, concept_list))
 
-def parse_valueset(json_file) -> List[Coding]:
-    """
-    :param json_file: ValueSet file, espeically those provided by NLM/ONC/VSAC
-    :return: list of codeable concepts (system, code, display) to include
-    """
-    valueset = load_json(os.path.join(pwd(), json_file))
-
-    parsed = list()
-
-    for include in valueset['compose']['include']:
-        _system = include['system']
-        if 'concept' in include.keys():
-            for concept in include['concept']:
-                concept['system'] = _system
-                parsed.append(Coding(concept))
-    return parsed
-
-def pwd(): return os.path.dirname(__file__)
+def get_path(filename=None):
+    if filename:
+        return os.path.join(os.path.dirname(__file__), filename)
+    else:
+        return os.path.dirname(__file__)
 
 
 if __name__ == "__main__":
-    icd10 = parse_valueset('htn_valueset_dx_icd10.json')
-    snomed = parse_valueset('htn_valueset_dx_snomed.json')
-    rxnorm = parse_valueset('htn_valueset_rx_rxnorm.json')
+    diagnosis = include_coding('htn_valueset_dx_icd10.json') + \
+                include_coding('htn_valueset_dx_snomed.json')
 
-    _sql = create_view('htn__define_dx', icd10+snomed)
-    print(_sql)
+    medication = include_coding('htn_valueset_rx_rxnorm.json')
 
-    _sql = create_view('htn__define_rx', rxnorm)
-    print(_sql)
+    vaccine = include_coding('core_valueset_vaccine_cvx_cste.json') +\
+              include_coding('core_valueset_vaccine_rxnorm_cste.json') + \
+              include_coding('core_valueset_vaccine_rxnorm_hl7.json')
+
+    frailty = include_coding('htn_valueset_frailty_cpt_ncqa.json') + \
+              include_coding('htn_valueset_frailty_icd10_ncqa.json') + \
+              include_coding('htn_valueset_frailty_hcpcs_ncqa.json') + \
+              include_coding('htn_valueset_frailty_snomed_ncqa.json')
+
+    write_view_sql('htn__define_dx', diagnosis)
+    write_view_sql('htn__define_rx', medication)
+    write_view_sql('htn__define_vaccine', vaccine)
+    write_view_sql('htn__define_frailty', frailty)
