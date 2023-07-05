129b17629e69af0a680eb185087e23bc99dc6e4d
comorbidity <andrew.mcmurry@tch.harvard.edu>
2023-07-05 11:25:24 -0700

    scaled back what we count.py

--- cumulus_library_hypertension/htn/count.py
+++ cumulus_library_hypertension/htn/count.py
@@ -9,13 +9,15 @@ def table(tablename: str, duration=None) -> str:
     else: 
         return f'{STUDY_PREFIX}__{tablename}'
 
-def count_bp(duration='month'):
+def count_bp(duration=None):
     view_name = table('count_bp', duration)
     from_table = table('bp')
-    cols = [f'obs_{duration}',
-            'enc_class_code',
+    cols = ['hypertension', 'hypotension', 'enc_class_code',
             'gender', 'age_at_visit', 'race_display', 'ethnicity_display',
             'hypertension', 'hypotension']
+
+    if duration:
+        cols.append(f'obs_{duration}')
 
     return counts.count_patient(view_name, from_table, cols)
 
@@ -35,11 +37,15 @@ def count_rx(duration='month'):
             'category_code', 'rx_display']
     return counts.count_patient(view_name, from_table, cols)
 
+def count_procedure():
+    view_name = table('count_procedure')
+    from_table = table('procedure')
+    cols = ['proc_display', 'proc_system']
+    return counts.count_encounter(view_name, from_table, cols)
 
 def concat_view_sql(create_view_list: List[str]) -> str:
     """
     :param create_view_list: SQL prepared statements
-    :param filename: path to output file, default 'count.sql' in PWD
     """
     seperator = '-- ###########################################################'
     concat = list()
@@ -60,7 +66,9 @@ def write_view_sql(view_list_sql: List[str], filename='count.sql') -> None:
 
 if __name__ == '__main__':
     write_view_sql([
+        count_bp(),
         count_bp('month'),
         count_dx('month'),
-        count_rx('month')
+        # count_rx('month'), TODO requires support for FHIR MedicationRequest
+        # count_procedure()  TODO requires support for FHIR Procedure
     ])
--- cumulus_library_hypertension/htn/count.sql
+++ cumulus_library_hypertension/htn/count.sql
@@ -1,18 +1,37 @@
 -- ###########################################################
+CREATE or replace VIEW htn__count_bp AS 
+    with powerset as
+    (
+        select
+        count(distinct subject_ref)   as cnt_subject
+        
+        , hypertension, hypotension, enc_class_code, gender, age_at_visit, race_display, ethnicity_display, hypertension, hypotension        
+        FROM htn__bp
+        group by CUBE
+        ( hypertension, hypotension, enc_class_code, gender, age_at_visit, race_display, ethnicity_display, hypertension, hypotension )
+    )
+    select
+          cnt_subject as cnt 
+        , hypertension, hypotension, enc_class_code, gender, age_at_visit, race_display, ethnicity_display, hypertension, hypotension
+    from powerset 
+    WHERE cnt_subject >= 10 
+    ORDER BY cnt desc;
+
+-- ###########################################################
 CREATE or replace VIEW htn__count_bp_month AS 
     with powerset as
     (
         select
         count(distinct subject_ref)   as cnt_subject
         
-        , obs_month, enc_class_code, gender, age_at_visit, race_display, ethnicity_display, hypertension, hypotension        
+        , hypertension, hypotension, enc_class_code, gender, age_at_visit, race_display, ethnicity_display, hypertension, hypotension, obs_month        
         FROM htn__bp
         group by CUBE
-        ( obs_month, enc_class_code, gender, age_at_visit, race_display, ethnicity_display, hypertension, hypotension )
+        ( hypertension, hypotension, enc_class_code, gender, age_at_visit, race_display, ethnicity_display, hypertension, hypotension, obs_month )
     )
     select
           cnt_subject as cnt 
-        , obs_month, enc_class_code, gender, age_at_visit, race_display, ethnicity_display, hypertension, hypotension
+        , hypertension, hypotension, enc_class_code, gender, age_at_visit, race_display, ethnicity_display, hypertension, hypotension, obs_month
     from powerset 
     WHERE cnt_subject >= 10 
     ORDER BY cnt desc;
@@ -32,25 +51,6 @@ CREATE or replace VIEW htn__count_dx_month AS
     select
           cnt_subject as cnt 
         , cond_month, enc_class_code, gender, age_at_visit, race_display, ethnicity_display, cond_display, cond_system_display
-    from powerset 
-    WHERE cnt_subject >= 10 
-    ORDER BY cnt desc;
-
--- ###########################################################
-CREATE or replace VIEW htn__count_rx_month AS 
-    with powerset as
-    (
-        select
-        count(distinct subject_ref)   as cnt_subject
-        
-        , authoredon_month, category_code, rx_display        
-        FROM htn__rx
-        group by CUBE
-        ( authoredon_month, category_code, rx_display )
-    )
-    select
-          cnt_subject as cnt 
-        , authoredon_month, category_code, rx_display
     from powerset 
     WHERE cnt_subject >= 10 
     ORDER BY cnt desc;
