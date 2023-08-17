6c8aa1258297273eecdf8e39b84ffcdbdfd2b294
Matt Garber <matthew.garber@childrens.harvard.edu>
2023-08-17 10:26:33 -0400

    Update for new condition table format

--- cumulus_library_hypertension/htn/table_comorbidity.sql
+++ cumulus_library_hypertension/htn/table_comorbidity.sql
@@ -9,21 +9,19 @@ WITH condition_row AS
     SELECT DISTINCT
         c.subject_ref,
         c.encounter_ref,
-        c.category.display as comorbidity_category_display,
-        cc.code AS comorbidity_code,
-        cc.display as comorbidity_display,
+        c.category_display as comorbidity_category_display,
+        c.code AS comorbidity_code,
+        c.code_display as comorbidity_display,
         fhirspec.define as comorbidity_system_display,
         c.recorded_month AS comorbidity_month,
         c.recorded_week AS comorbidity_week,
         c.recordeddate as comorbidity_date
     FROM
-        core__condition_codable_concepts cc,
         core__condition AS c,
         core__fhir_define as fhirspec
     WHERE
-        cc.id = c.condition_id          and
-        cc.code_system = fhirspec.url   and
-        cc.code not in (select distinct code from htn__define_dx)
+        c.code_system = fhirspec.url   and
+        c.code not in (select distinct code from htn__define_dx)
 )
 select distinct
         condition_row.*,
